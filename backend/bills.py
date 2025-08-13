5117f31067de7fbf156dd85b6ad89713978d0531
patopoco <papopoco@gmail.com>
2025-08-13 13:58:42 +0200

    feat: another work on databses: bills, suppliers

--- backend/bills.py
+++ backend/bills.py
@@ -1,33 +1,86 @@
+from typing import Union
 import helper_functions
+from psycopg2._psycopg import cursor, connection, Error, Warning
+
+
+INSERT_QUERY = '''INSERT INTO bills (supplier, price, date)
+                VALUES (%s, %s, %s);'''
+
+CREATE_QUERY = '''CREATE TABLE IF NOT EXISTS bills (id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+                                                    supplier VARCHAR(40), 
+                                                    price REAL, 
+                                                    date DATE);'''
+
+DELETE_QUERY = '''DELETE FROM bills WHERE id = %s;'''
+
+SELECT_QUERY = '''SELECT * FROM bills WHERE id = %s;'''
+
+SELECT_COLUMN_QUERY = '''SELECT %s FROM bills WHERE id = %s;'''
+
 
 #inserts row into the table bills
-def insert():
-    return f'''INSERT INTO bills (supplier, price, date)
-                VALUES (%s, %s, %s);'''
+def insert(db_cursor: cursor) -> bool:
+    try:
+        query = INSERT_QUERY
+        db_cursor.execute(query, ())
+
+    except Warning as e:
+        print(e.__class__.__name__)
+        return False
+    except Error as e:
+        print(f"{e.__class__.__name__} : {e.pgerror}")
+        return False
+    return True
 
 
 #creates new table bills with columns: id, supplier, price, date
-def create():
-    return '''CREATE TABLE IF NOT EXISTS bills (id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-                                                supplier VARCHAR(40), 
-                                                price REAL, 
-                                                date DATE)'''
+def create(db_cursor: cursor) -> bool:
+    try:
+        query = CREATE_QUERY
+        db_cursor.execute(query, ())
+
+    except Warning as e:
+        print(e.__class__.__name__)
+        return False
+    except Error as e:
+        print(f"{e.__class__.__name__} : {e.pgerror}")
+        return False
+    return True
 
 
 #deletes from table bills
-def delete(id):
-    return f'''DELETE FROM bills WHERE id = {id}'''
+def delete(db_cursor:cursor, id_num: int) -> bool:
+    try:
+        query = DELETE_QUERY
+        db_cursor.execute(query, (id_num,))
+
+    except Warning as e:
+        print(e.__class__.__name__)
+        return False
+    except Error as e:
+        print(f"{e.__class__.__name__} : {e.pgerror}")
+        return False
+    return True
 
 
 #return the whole row of table bills where the id matches
-def select(id):
-    return f'''SELECT * FROM bills WHERE id = {id}'''
+def select(db_cursor: cursor, id_num: int) -> tuple[int, str, float, str] | None:
+    try:
+        query = INSERT_QUERY
+        db_cursor.execute(query, (id_num,))
+        row = db_cursor.fetchone()
+    except Warning as e:
+        print(e.__class__.__name__)
+        return None
+    except Error as e:
+        print(f"{e.__class__.__name__} : {e.pgerror}")
+        return None
+    return row
 
 
 #return a specific value from table bills
-def get_column(cursor, id, column):
+def get_column(db_cursor: cursor, id_num: int, column: str) -> int | str | float | None:
     allowed_columns = ['supplier', 'price', 'id', 'date']
     if column not in allowed_columns:
         return None
-    query = f'''SELECT {column} FROM bills WHERE id = {id}'''
-    return helper_functions.get_column_value(cursor, query)
+    return helper_functions.get_column_value(db_cursor, SELECT_COLUMN_QUERY, column, id_num)
--- backend/helper_functions.py
+++ backend/helper_functions.py
@@ -1,7 +1,16 @@
-def get_column_value(cursor, query):
+from psycopg2._psycopg import cursor, connection, Error, Warning
+
+def get_column_value(db_cursor: cursor, query, column: str, id: int) \
+                                            -> int | float | str | None:
     if not query:
         return None
-
-    cursor.execute(query)
-    row = cursor.fetchone()
+    try:
+        db_cursor.execute(query, (column, id))
+        row = db_cursor.fetchone()
+    except Warning as e:
+        print(e.__class__.__name__)
+        return None
+    except Error as e:
+        print(f"{e.__class__.__name__} : {e.pgerror}")
+        return None
     return row[0] if row else None
