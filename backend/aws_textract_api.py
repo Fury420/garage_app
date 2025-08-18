1f2235b4ff3bb21657a4343571aeeb5c1bc263f0
Jan Durec <durecjan223@gmail.com>
2025-08-18 11:44:12 +0200

    refactor: split convert_and_compress method to 2 methods, first one handling compression of images (conversion to JPEG is a byproduct), second one handling pdf documents

--- backend/aws_textract_api.py
+++ backend/aws_textract_api.py
@@ -6,6 +6,8 @@ from botocore.config import Config
 from botocore.exceptions import BotoCoreError
 from PIL import Image
 from pdf2image import convert_from_bytes
+from PyPDF2 import PdfReader
+from typing import Union
 import json
 
 AWS_CREDENTIALS_FILE = "./aws_credentials.json"
@@ -44,47 +46,68 @@ def get_textract_client() -> client:
     )
 
 
-def convert_and_compress(file_bytes: bytes, is_pdf: bool=False, pdf_page: int=1) -> bytes | None:
+def pdf_to_jpeg(document_bytes: bytes) -> list[bytes] | None:
     """
-    Converts PDF or image document to compressed JPEG image. If there is any issue with
-    converting given document or final compressed document is larger than MAX_IMAGE_SIZE,
-    method prints the exception to stdout and returns None.
-    :param file_bytes: binary representation of PDF or image document
-    :param is_pdf: specifies whether document is in PDF format
-    :param pdf_page: specifies which PDF page we want to convert and compress
-    :return: JPEG image in binary representation
+    Converts given PDF document to JPEG images using pdf2image library. If conversion or image
+    compression fails, method returns None.
+    :param document_bytes: binary representation of PDF document
+    :return: Compressed JPEG images in binary representation
     """
-    # TODO: split method to image and pdf compression (we know content type from upload file)
-    if is_pdf:
+    pdf_reader = PdfReader(BytesIO(document_bytes))
+    page_count = pdf_reader.getNumPages()
+    try:
+        images = convert_from_bytes(
+            document_bytes,
+            first_page=1,
+            last_page=page_count,
+            dpi=200,
+            fmt="jpeg"
+        )
+        if not images:
+            raise ValueError("PDF conversion failed")
+        compressed_images = []
+        for image in images:
+            compressed_images.append(compress_image(image))
+            if compressed_images[-1] is None:
+                raise ValueError(f"Compression failed on page: {len(compressed_images)}")
+        return compressed_images
+    except Exception as e:
+        print(f"{e.__class__.__name__} : {e.args}")
+        return None
+
+
+def compress_image(image: Union[Image.Image | bytes]) -> bytes | None:
+    """
+    Compression of images using Pillow library. Image can be passed using binary representation [bytes] or
+    the Pillow Image handle. The compression consists of converting image mode to RGB, resampling to
+    1080x1920 resolution and finally JPEG compression [quality=85]. If any of said operations cause an exception,
+    given image is not supported by Pillow library or the final compressed image is larger than MAX_IMAGE_SIZE,
+    method returns None. The image handle is always closed by this method.
+    :param image: image handle or binary representation of said image
+    :return: Compressed JPEG image in binary representation.
+    """
+    if isinstance(image, bytes):
         try:
-            images = convert_from_bytes(
-                file_bytes,
-                first_page=pdf_page,
-                last_page=pdf_page,
-                dpi=200,
-                fmt="jpeg"
-            )
-            if not images:
-                raise ValueError("PDF conversion failed")
-            pil_image = images[pdf_page - 1]
+            pil_image = Image.open(BytesIO(image))
         except Exception as e:
             print(f"{e.__class__.__name__} : {e.args}")
             return None
     else:
-        try:
-            pil_image = Image.open(BytesIO(file_bytes))
-            if pil_image.mode in ("RGBA", "P", "LA"):
-                pil_image = pil_image.convert("RGB")
-        except Exception as e:
-            print(f"{e.__class__.__name__} : {e.args}")
-            return None
-    pil_image.thumbnail((1080, 1920), Image.Resampling.LANCZOS)
-    out_buffer = BytesIO()
-    pil_image.save(out_buffer, format="JPEG", quality=85)
-    print(out_buffer.getbuffer().nbytes)
-    if out_buffer.getbuffer().nbytes >= MAX_IMAGE_SIZE:
+        pil_image = image
+    try:
+        if pil_image.mode in ("RGBA", "P", "LA"):
+            pil_image = pil_image.convert("RGB")
+        pil_image.thumbnail((1080, 1920), Image.Resampling.LANCZOS)
+        out_buffer = BytesIO()
+        pil_image.save(out_buffer, format="JPEG", quality=85)
+    except Exception as e:
+        print(f"{e.__class__.__name__} : {e.args}")
         return None
-    return out_buffer.getvalue()
+    finally:
+        pil_image.close()
+    if out_buffer.getbuffer().nbytes < MAX_IMAGE_SIZE:
+        return out_buffer.getvalue()
+    return None
 
 
 def test_run() -> None:
@@ -94,13 +117,13 @@ def test_run() -> None:
     textract_client = get_textract_client()
     try:
         with open(TEST_INVOICE_PATH, "rb") as test_file:
-            jpeg_bytes = convert_and_compress(test_file.read())
+            payload = compress_image(test_file.read())
         response = textract_client.analyze_expense(
             Document={
-                "Bytes": b64encode(jpeg_bytes).decode('utf-8')
+                "Bytes": b64encode(payload).decode('utf-8')
             }
         )
-        with open("./test_run_out.json", "w") as out_file:
+        with open(f"./test_run{datetime.now().isoformat()}.json", "w") as out_file:
             json.dump(response, out_file)
     except OSError as e:
         print(f"{e.__class__.__name__} : {e.args}")
@@ -111,14 +134,49 @@ def test_run() -> None:
 if __name__ == '__main__':
     """
     test_run()
-    """
+    
+    
+    # should be global in the api implementation
+    textract_client = get_textract_client()
+
+    # we will receive an UploadFile from frontend instead
+    filepath = "../test_api_dir/test_data/homegym.pdf"
+
+    # api call should look similar to this:
     try:
-        with open("./test_data/vlcie_sirupy.pdf", "rb") as in_file:
-            filebytes = in_file.read()
-        compressed_file = convert_and_compress(filebytes, is_pdf=True)
-        if compressed_file is None:
-            raise ValueError("Compression failed")
-        with open(f"./compression_out_{datetime.now().time().isoformat()}.jpeg", "wb") as out_file:
-            out_file.write(compressed_file)
-    except OSError as e:
+        with open(filepath, "rb") as test_file:
+            # obtain binary representation (we will get this with fastapi.UploadFile)
+            file_bytes = test_file.read()
+
+            # here we would call fastapi.UploadFile.content_type to determine type
+            # for now we will use postfix to determine type
+            if test_file.name.endswith(".pdf"):
+                # if given document is in pdf format we must first convert and compress it
+                compressed_images = pdf_to_jpeg(file_bytes)
+
+                # now we proceed send every pdf page to textract service
+                for image_bytes in compressed_images:
+                    response = textract_client.analyze_expense(
+                        Document={
+                            "Bytes": b64encode(image_bytes).decode('utf-8')
+                        }
+                    )
+                    # response should be json serializable - TODO save it, process it,...
+
+            elif test_file.name.endswith((".jpeg", ".jpg", ".png", ".bmp", ".tiff")):
+                # if given document is in image format supported by Pillow library we just need to compress it
+                image_bytes = compress_image(file_bytes)
+
+                # now we proceed to send compressed invoice to textract service
+                response = textract_client.analyze_expense(
+                    Document={
+                        "Bytes": b64encode(image_bytes).decode('utf-8')
+                    }
+                )
+                # response should be json serializable - TODO save it, process it,...
+            else:
+                raise ValueError("HTTP error code : invalid file format given")
+    except Exception as e:
         print(f"{e.__class__.__name__} : {e.args}")
+        # return appropriate HTTP response to frontend
+    """
